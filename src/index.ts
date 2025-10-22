22777a87f03a9b824d86c071483d43c43aa9180f
yuzili <395345203@qq.com>
2025-10-22 16:33:54 +0800

    feat: webview demo.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { defineExtension } from 'reactive-vscode'
 import { window } from 'vscode'
+import { useDemoWebviewView } from './views/content'
 
 const { activate, deactivate } = defineExtension(() => {
   window.showInformationMessage('Hello')
+  useDemoWebviewView()
 })
 
 export { activate, deactivate }
---
+++ src/views/content.ts
@@ -0,0 +1,38 @@
+import { computed, defineService, ref, useWebviewView } from 'reactive-vscode'
+
+export const useDemoWebviewView = defineService(() => {
+  const message = ref('')
+  const html = computed(() => `
+  <script>
+    vscode = acquireVsCodeApi()
+    function updateMessage() {
+      vscode.postMessage({
+        type: 'updateMessage',
+        message: document.querySelector('input').value,
+      })
+    }
+  </script>
+  <p>${message.value}</p>
+  <div style="display:flex; flex-wrap:wrap;">
+    <input type="text" placeholder="Input Message" />
+    <button onclick="updateMessage()">Update Message</button>
+  </div>
+  `)
+
+  const { postMessage } = useWebviewView(
+    'jssdk-doc-assistant',
+    html,
+    {
+      webviewOptions: {
+        enableScripts: true,
+        enableCommandUris: true,
+      },
+      onDidReceiveMessage(ev) {
+        if (ev.type === 'updateMessage')
+          message.value = ev.message
+      },
+    },
+  )
+
+  return { message, postMessage }
+})
