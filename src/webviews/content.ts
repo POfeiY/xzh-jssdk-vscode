41564e4ffb155b41cd3a8a1e887b21e3a8c46895
yuzili <395345203@qq.com>
2025-10-28 21:02:01 +0800

    feat: update getHtmlForWebview.

--- src/webviews/content.ts
+++ src/webviews/content.ts
@@ -1,4 +1,4 @@
-import { computed, defineService, ref, useFileUri, useWebviewView } from 'reactive-vscode'
+import { computed, defineService, extensionContext, ref, useFileUri, useWebviewView } from 'reactive-vscode'
 import vscode from 'vscode'
 
 export const useDemoWebviewView = defineService(() => {
@@ -12,7 +12,9 @@ export const useDemoWebviewView = defineService(() => {
       webviewOptions: {
         enableScripts: true,
         enableCommandUris: true,
-        localResourceRoots: [],
+        localResourceRoots: [
+
+        ],
       },
       onDidReceiveMessage(ev) {
         if (ev.type === 'updateMessage')
@@ -21,32 +23,43 @@ export const useDemoWebviewView = defineService(() => {
     },
   )
 
-  if (view.value?.webview)
-    view.value!.webview.html = getHtmlForWebview(view.value!.webview)
+  setTimeout(() => {
+    vscode.window.showInformationMessage(getHtmlForWebview(view.value!.webview))
+    if (view.value?.webview)
+      view.value!.webview.html = getHtmlForWebview(view.value!.webview)
+  }, 1000)
 
   return { message, postMessage, view }
 })
 
+function getNonce() {
+  let text = ''
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+  return text
+}
+
 function getHtmlForWebview(view: vscode.Webview) {
-  vscode.window.showInformationMessage('getHtmlForWebview')
-  const root = useFileUri('')
-  const entryScript = view.asWebviewUri(vscode.Uri.joinPath(root.value, 'res/webapp/static/js/lib-vue.2f9000e5.js'))
-  const entryScript1 = view.asWebviewUri(vscode.Uri.joinPath(root.value, 'res/webapp/static/js/index.3a6ae001.js'))
-  const cssFile = view.asWebviewUri(vscode.Uri.joinPath(root.value, 'res/webapp/static/css/index.677e40e0.css'))
+  const nonce = getNonce()
+  const entryScript = view.asWebviewUri(useFileUri(vscode.Uri.joinPath(__dirname, 'res/webapp/static/js/lib-vue.2f9000e5.js')))
+  const entryScript1 = view.asWebviewUri(useFileUri(vscode.Uri.joinPath(__dirname, 'res/webapp/static/js/index.3a6ae001.js')))
+  const cssFile = view.asWebviewUri(useFileUri(vscode.Uri.joinPath(__dirname, 'res/webapp/static/css/index.677e40e0.css')))
   return `<!doctype html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
+    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${view.cspSource}; script-src 'nonce-${nonce}';">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>webapp</title>
-    <script defer src=${entryScript}></script>
-    <script defer src=${entryScript1}></script>
-    <link href=${cssFile} rel="stylesheet">
+    <script defer nonce=${nonce} src=${entryScript}></script>
+    <script defer nonce=${nonce} src=${entryScript1}></script>
+    <link href=${cssFile} nonce=${nonce} rel="stylesheet">
   </head>
   <body>
     <div id="app"></div>
   </body>
 </html>
-
 `
 }
